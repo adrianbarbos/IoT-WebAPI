c14348592a40e1c18efde02cb05b4ecab3244191
adrianbarbos <adrianbarbos@Adrians-MacBook-Pro.local>
2015-04-08 09:30:18 +0300

    config update

--- config/config.php
+++ config/config.php
@@ -1,7 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'iot');
-
+define('DB_USER', "adi"); // db user
+define('DB_PASSWORD', "6NxgkbR2E9Mz"); // db password (mention your db password here)
+define('DB_DATABASE', "adi"); // database name
+define('DB_SERVER', "localhost"); // db server
--- config/gcm/config.php
+++ config/gcm/config.php
@@ -10,6 +10,6 @@ define("DB_DATABASE", "u572856767_gcm");
 /*
  * Google Cloud Messaging API Key
  */
-define("GOOGLE_API_KEY", "AIzaSyDiS35zz_I1g8qfIeSCafL8fL8fMXO1wJk"); // Place your Google API Key
+define("GOOGLE_API_KEY", "AIzaSyDP0iiV7iGbugKKZehHlsVkdB3Okl_KOwQ"); // Place your Google API Key
                           
 ?>
